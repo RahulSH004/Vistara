32c89e80c29e4c26c0708dba7430ccd045ac43c9
RahulSH004 <rs739025@gmail.com>
2026-03-13 18:15:26 +0000

    rooms routes

--- src/modules/rooms/rooms_routes.ts
+++ src/modules/rooms/rooms_routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createRoomcontroller } from "./rooms_controller.js";
+import { authmiddleware } from "../auth/auth_middleware.js";
 
 const roomrouter = Router()
 
-roomrouter.post("hotels/:hotel_id/rooms", createRoomcontroller);
+roomrouter.post("hotels/:hotel_id/rooms",authmiddleware,createRoomcontroller);
 
 export default roomrouter;
