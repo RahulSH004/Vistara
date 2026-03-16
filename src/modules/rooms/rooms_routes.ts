2e267901b0cf09943e37aed0c40649b57ccb574e
RahulSH004 <rs739025@gmail.com>
2026-03-16 10:40:08 +0000

    rooms routes add middleware

--- src/modules/rooms/rooms_routes.ts
+++ src/modules/rooms/rooms_routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { createRoomcontroller } from "./rooms_controller.js";
 import { authmiddleware } from "../auth/auth_middleware.js";
+import { requireRole } from "../../middleware/role_middleware.js";
+import { requireHotelowner } from "./rooms_middleware.js";
 
 const roomrouter = Router()
 
-roomrouter.post("hotels/:hotel_id/rooms",authmiddleware,createRoomcontroller);
+roomrouter.post("hotels/:hotel_id/rooms",
+    authmiddleware,
+    requireRole('admin'),
+    requireHotelowner,
+    createRoomcontroller
+);
 
 export default roomrouter;
