47821ae8c132ec4256e04e63f72ba74265c70e22
RahulSH004 <rs739025@gmail.com>
2026-03-16 10:38:08 +0000

    create rooms middleware

--- src/modules/rooms/rooms_middleware.ts
+++ src/modules/rooms/rooms_middleware.ts
@@ -1,1 +1,34 @@
+//implement middleware the admin/owner can only create rooms for hotel they own 
 import { Request, Response, NextFunction } from "express";
+import { ApiError } from "../../utils/ApiError.js";
+import { db } from "../../db/connection.js";
+import { hotels } from "../../db/schema.js";
+import { eq } from "drizzle-orm";
+
+
+export async function requireHotelowner(
+    req: Request,
+    res: Response,
+    next: NextFunction
+){
+    try {
+        const user = req.user;
+        const hotelId = req.params.hotelId || req.body.hotel_id;
+
+        if(!user) throw new ApiError(401, "Unauthenticated");
+        if(!hotelId) throw new ApiError(400, "Hotel_ID Required");
+
+        const [hotel] = await db
+            .select({ownerId: hotels.owner_id})
+            .from(hotels)
+            .where(eq(hotels.id, hotelId))
+            .limit(1);
+        
+        if(!hotel || hotel.ownerId !== user.id){
+            throw new ApiError(403, "Not Hotel Ownwer")
+        }
+        next();
+    }catch(error){
+        next(error);
+    }
+}
